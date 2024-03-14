69c6b1917da369815e6c8395e2db5b98c30138a4
Bryan <brianforonda94@gmail.com>
2024-03-13 19:14:00 -0500

    dependencies gh-pages

--- src/pages/translate-app/TranslateApp.tsx
+++ src/pages/translate-app/TranslateApp.tsx
@@ -1,9 +1,12 @@
-import backImg  from '../../assets/img/hero_img.jpg';
 import { Translate } from '../../styled-components/trasnlateApp';
+import logoImg  from '../../assets/img/logo.svg';
 
 export const TranslateApp = () => {
     return (
       <Translate>
+        <div className='container__logo'>
+          <img src={logoImg} alt="logo-dev" />
+        </div>
       </Translate>
     )
   }
--- src/styled-components/trasnlateApp.tsx
+++ src/styled-components/trasnlateApp.tsx
@@ -2,8 +2,13 @@ import styled from 'styled-components';
 import backImg from '../assets/img/hero_img.jpg';
 
 export const Translate = styled.div`
-    background-image: url(${backImg});
-    background-size: cover;
-    background-position: center;
-    min-height: 100vh;
+  background-image: url(${backImg});
+  background-size: cover;
+  background-position: center;
+  min-height: 100vh;
+
+  .container__logo {
+    text-align: center;
+    padding-top: 80px;
+  }
 `;
